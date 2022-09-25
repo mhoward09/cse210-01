f6606a56ef32d9f71bb280028e152f20e50fc0c1
Melissa Howard <how20034@byui.edu>
2022-09-24 21:13:56 -0600

    established initial board layout

--- cse210-01.py
+++ cse210-01.py
@@ -4,7 +4,8 @@
 # main function of the game, when it runs the game is played
 def main():
     squares = placeHolders()
-    print(squares)
+    #print(squares)
+    grid(squares)
 
 # initializes array of place holders for the squares that can be chosen
 def placeHolders():
@@ -13,5 +14,15 @@ def placeHolders():
         placeHolders.append(number + 1)
     return placeHolders
 
+#initalizes tic-tac-toe grid
+def grid(placeHolders):
+    print()
+    print(placeHolders[0]," | ",placeHolders[1], " | ", placeHolders[2])
+    print("---+-----+---")
+    print(placeHolders[3]," | ",placeHolders[4], " | ", placeHolders[5])
+    print("---+-----+---")
+    print(placeHolders[6]," | ",placeHolders[7], " | ", placeHolders[8])
+
+#calls and runs main function
 if __name__ == "__main__":
     main()
